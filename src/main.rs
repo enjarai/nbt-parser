fb58419cc87286f7dbdd14b79e151784caf54b03
enjarai <enjarai@protonmail.com>
2023-03-22 11:45:35 +0100

    Gzip option

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 mod parser;
+mod getters;
 
 use std::env::args;
 use std::fs::File;
@@ -7,10 +8,24 @@ use flate2::read::GzDecoder;
 
 fn main() {
     let file = args().nth(1).unwrap();
+    let gzip = match args().nth(2) {
+        Some(s) => s == "gzip",
+        None => false
+    };
     match File::open(file) {
         Ok(data) => {
-            let mut reader = BufReader::new(GzDecoder::new(data));
-            let tag = parser::Tag::parse(&mut reader).unwrap();
+            let time = std::time::Instant::now();
+
+            let tag = if gzip {
+                let mut reader = BufReader::new(GzDecoder::new(data));
+                parser::Tag::parse(&mut reader).unwrap()
+            } else {
+                let mut reader = BufReader::new(data);
+                parser::Tag::parse(&mut reader).unwrap()
+            };
+
+            println!("Took {}ms", time.elapsed().as_millis());
+            println!();
             println!("{}", tag);
         },
         Err(e) => println!("{e}")
