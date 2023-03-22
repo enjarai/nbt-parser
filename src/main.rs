4a53d311681dcba16aad0097d1bd18c01ecdc3b9
enjarai <enjarai@protonmail.com>
2023-03-22 11:33:05 +0100

    It works even better now

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,16 @@ mod parser;
 
 use std::env::args;
 use std::fs::File;
-use std::io::{BufRead, BufReader, Read};
+use std::io::BufReader;
+use flate2::read::GzDecoder;
 
 fn main() {
     let file = args().nth(1).unwrap();
     match File::open(file) {
         Ok(data) => {
-            let mut reader = BufReader::new(data);
+            let mut reader = BufReader::new(GzDecoder::new(data));
             let tag = parser::Tag::parse(&mut reader).unwrap();
-            println!("{:?}", tag);
+            println!("{}", tag);
         },
         Err(e) => println!("{e}")
     }
--- src/parser.rs
+++ src/parser.rs
@@ -1,38 +1,16 @@
 use std::collections::HashMap;
-use std::error::Error;
-use std::io::{BufReader, ErrorKind, Read};
-use std::string::{FromUtf8Error, ParseError};
-use std::{io, u16};
+use std::io::{BufReader, Read};
+use std::string::FromUtf8Error;
+use std::io;
 use std::fmt::{Debug, Display, Formatter};
-use std::mem::transmute;
-use byteorder::{BigEndian, ByteOrder, ReadBytesExt, WriteBytesExt};
-use crate::parser::Tag::{ByteTag, ByteArray, DoubleTag, EndTag, FloatTag, IntTag, LongTag, ShortTag, StringTag, ListTag, CompoundTag, IntArray};
+use byteorder::{BigEndian, ReadBytesExt};
 
-// struct Parser<'a> {
-//     data: &'a Vec<u8>,
-//     i: usize
-// }
-//
-// impl<'a> Parser<'a> {
-//     fn new(data: &Vec<u8>) -> Parser {
-//         Parser {
-//             data,
-//             i: 0
-//         }
-//     }
-//
-//     fn eat(&mut self, count: usize) -> &Vec<u8> {
-//         let output = self.data[i..i+count];
-//         self.i += count;
-//         output
-//     }
-// }
+use crate::parser::Tag::{ByteTag, ByteArray, DoubleTag, EndTag, FloatTag, IntTag, LongTag, ShortTag, StringTag, ListTag, CompoundTag, IntArray, LongArray};
 
 #[derive(Debug)]
 pub enum TagParseErr {
     InvalidType,
-    InvalidLength,
-    InvalidUtf8,
+    InvalidUtf8(FromUtf8Error),
     IoError(io::Error)
 }
 
@@ -43,8 +21,8 @@ impl From<io::Error> for TagParseErr {
 }
 
 impl From<FromUtf8Error> for TagParseErr {
-    fn from(_: FromUtf8Error) -> TagParseErr {
-        TagParseErr::InvalidUtf8
+    fn from(err: FromUtf8Error) -> TagParseErr {
+        TagParseErr::InvalidUtf8(err)
     }
 }
 
@@ -73,36 +51,45 @@ impl Tag {
     fn parse_type(data_type: u8, buf: &mut BufReader<impl Read>) -> Result<Tag, TagParseErr> {
         match data_type {
             0x00 => Ok(EndTag),
-            0x01 => unsafe {
+            // Byte (1 byte)
+            0x01 => {
                 Ok(ByteTag(buf.read_i8()?))
             },
+            // Short (2 bytes)
             0x02 => {
                 Ok(ShortTag(buf.read_i16::<BigEndian>()?))
             },
+            // Int (4 bytes)
             0x03 => {
                 Ok(IntTag(buf.read_i32::<BigEndian>()?))
             },
+            // Long (8 bytes)
             0x04 => {
                 Ok(LongTag(buf.read_i64::<BigEndian>()?))
             },
+            // Float (4 bytes)
             0x05 => {
                 Ok(FloatTag(buf.read_f32::<BigEndian>()?))
             },
+            // Double (8 bytes)
             0x06 => {
                 Ok(DoubleTag(buf.read_f64::<BigEndian>()?))
             },
-            // 0x07 => {
-            //     let length = i32::from_be_bytes(*eat(&mut data, 4)?);
-            //     let mut result = Vec::new();
-            //     for b in data[0..length] {
-            //         result.push(i8::from_be_bytes(b))
-            //     }
-            //     Ok(ByteArray(result))
-            // },
+            // Byte Array (i32 length, then length bytes)
+            0x07 => {
+                let length = buf.read_i32::<BigEndian>()?;
+                let mut result = Vec::new();
+                for _ in 0..length {
+                    result.push(buf.read_i8()?)
+                }
+                Ok(ByteArray(result))
+            },
+            // String (u16 length, then length bytes)
             0x08 => {
                 let length = buf.read_u16::<BigEndian>()?;
-                Ok(StringTag(String::from_utf8(eat(buf, length as usize)?)?))
+                Ok(StringTag(String::from_utf8_lossy(&eat(buf, length as usize)?).parse().unwrap()))
             },
+            // List (u8 type, i32 length, then length tags of type)
             0x09 => {
                 let list_type = buf.read_u8()?;
                 let length = buf.read_i32::<BigEndian>()?;
@@ -112,6 +99,7 @@ impl Tag {
                 }
                 Ok(ListTag(result))
             },
+            // Compound (u8 type, u16 name length, name of length, then tag of type, repeat until type is 0)
             0x0A => {
                 let mut result = HashMap::new();
                 loop {
@@ -127,14 +115,22 @@ impl Tag {
                 }
                 Ok(CompoundTag(result))
             },
-            // 0x0B => {
-            //     let length = i32::from_be_bytes(*eat(&mut data, 4)?);
-            //     let mut result = Vec::new();
-            //     for b in data[0..length] {
-            //         result.push(i32::from_be_bytes(b))
-            //     }
-            //     Ok(IntArray(result))
-            // },
+            0x0B => {
+                let length = buf.read_i32::<BigEndian>()?;
+                let mut result = Vec::new();
+                for _ in 0..length {
+                    result.push(buf.read_i32::<BigEndian>()?)
+                }
+                Ok(IntArray(result))
+            },
+            0x0C => {
+                let length = buf.read_i32::<BigEndian>()?;
+                let mut result = Vec::new();
+                for _ in 0..length {
+                    result.push(buf.read_i64::<BigEndian>()?)
+                }
+                Ok(LongArray(result))
+            },
             _ => Err(TagParseErr::InvalidType)
         }
     }
@@ -144,4 +140,44 @@ fn eat(buf: &mut BufReader<impl Read>, length: usize) -> io::Result<Vec<u8>> {
     let mut result = vec![0u8; length];
     buf.read_exact(&mut result)?;
     Ok(result)
+}
+
+impl Display for Tag {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        match self {
+            EndTag => write!(f, "EndTag"),
+            ByteTag(value) => Display::fmt(value, f),
+            ShortTag(value) => Display::fmt(value, f),
+            IntTag(value) => Display::fmt(value, f),
+            LongTag(value) => Display::fmt(value, f),
+            FloatTag(value) => Display::fmt(value, f),
+            DoubleTag(value) => Display::fmt(value, f),
+            ByteArray(value) => list_fmt(value, f),
+            StringTag(value) => Display::fmt(value, f),
+            ListTag(value) => list_fmt(value, f),
+            CompoundTag(value) => {
+                write!(f, "{{")?;
+                for (i, (key, tag)) in value.iter().enumerate() {
+                    if i != 0 {
+                        write!(f, ", ")?;
+                    }
+                    write!(f, "{}: {}", key, tag)?;
+                }
+                write!(f, "}}")
+            }
+            IntArray(value) => list_fmt(value, f),
+            LongArray(value) => list_fmt(value, f)
+        }
+    }
+}
+
+fn list_fmt<T: Display>(value: &Vec<T>, f: &mut Formatter<'_>) -> std::fmt::Result {
+    write!(f, "[")?;
+    for (i, t) in value.iter().enumerate() {
+        if i != 0 {
+            write!(f, ", ")?;
+        }
+        write!(f, "{}", t)?;
+    }
+    write!(f, "]")
 }
